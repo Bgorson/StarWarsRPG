33cbd5de0eb4fec87a2bd84369c516fa7c306307
Brandon Gorson <42119471+Bgorson@users.noreply.github.com>
2018-12-18 14:56:50 -0600

    Removed Console Logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,7 +41,6 @@ $(data).each(function(i, e){
     <div class="card-body text-primary"><h5 class="card-title"></h5>\
     <img src="' + e.image + '"></div>\
     <div class="HP card-footer bg-transparent">HP: ' + e.health + '</div></div>')
-    //  $(".roster").append("<li class='portrait' ' data-attack='" +e.attack + "' counterAttack='" + e.counterAttack + "'health=" + e.health + "><p class='name'>"+e.name+"</p><img src='" + e.image + "'><p class='hp'>HP:"+e.health+ "</p></li>")
     winCondition = i;
         
     })
@@ -52,7 +51,6 @@ var firstPick= true;
 var noPicks = false;
 var heroPick =false;
 var $characters = $(".portrait");
-// var $characters = $(".roster li img");
 var allyAtt;
 var boostAllyAtt= allyAtt;
 var allyHP;
@@ -69,9 +67,6 @@ $characters.on("click", function(){//ENEMIES
     enemyAtt = $(this).attr("counterAttack");
     enemyHP = $(this).attr("health");
     $(this).appendTo('.enemySide')
-    console.log("enemy att is" + enemyAtt);
-    console.log(enemyHP);
-    console.log("wins needed"+ winCondition)
     noPicks = true;
     enemyPick= true;
     
@@ -81,11 +76,9 @@ $characters.on("click", function(){//ENEMIES
 
 $characters.on("click", function(){//ALLY
     if (firstPick == true && noPicks== false && heroPick == false && gameOver==false){
-        console.log(this)
-        allyAtt = $(this).attr("data-attack");
+         allyAtt = $(this).attr("data-attack");
         boostAllyAtt = $(this).attr("data-attack");
         allyHP = $(this).attr("health");
-        console.log("ally hp is"+ allyHP);
         firstPick = !firstPick;
         $characters.appendTo('.opponents');
         $(this).appendTo('.allyFightSide');
@@ -98,16 +91,13 @@ $characters.on("click", function(){//ALLY
     }
  
 })
-//target footer and re-write
-$(".attackBtn").on("click", function(){
+
+$(".attackBtn").on("click", function(){ //attack button event
     if (gameOver==false && enemyPick == true){
     $(".allyAttacks").text("You attack for " + boostAllyAtt + " Damage")
     $(".enemyAttacks").text("You are counter-attacked for " + enemyAtt + " Damage")
     allyHP= parseInt(allyHP)-parseInt(enemyAtt);
     enemyHP= parseInt(enemyHP)- parseInt(boostAllyAtt);
-    console.log("enemy hp" + enemyHP)
-    console.log("inc attack" + boostAllyAtt)
-    console.log("OG attack" + allyAtt)
     $(".allyFightSide .HP").text("HP:" + allyHP);
     $(".enemySide .HP").text("HP:" + enemyHP);
     boostAllyAtt = parseInt(boostAllyAtt) + parseInt(allyAtt);
@@ -134,29 +124,3 @@ $(".attackBtn").on("click", function(){
     }
 }
 })
-
-
-
-    
-
-
-  /* Logic to be developed:
-  When I click a character- he becomes my character,
-  Others move down and are selectable now.
-  once I select my opponent- I click attack
-  Health is decreased by attacks and counter attacks
-  Attack builds on my character
-  Once HP hits 0- attack choosing phase starts.
-  Game ends when I get 0 hp or no more opponents (3 wins)
-console.log($(this).attr("data-attack"));
-  Questions:
-  do I want each character to be unique
-  What is the logic for moving characters, CSS manipulation?
-  How do I limit what is a clickable event? 
-  */
-
-  /*
-  Still to be developed:
-  Restarting the game
-  cleaning up interface
-  */
